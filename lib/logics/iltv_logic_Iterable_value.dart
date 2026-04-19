9ca9c26e4cba8fdff6d1eae7c38dd0f077c56b6b
isx <globax58@yandex.ru>
2026-04-20 01:10:27 +0300

    - added iterable logic values

---
+++ lib/logics/iltv_logic_Iterable_value.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:iltv/logics/iltv_logic_value_read_only.dart';
+
+class IltvLogicIterableValue<T extends Iterable<E>, E> extends IltvLogicValueReadOnly<T>{
+  set value(T newValue) {
+    isInitialized = true;
+    super.innerValue = newValue;
+    if(onUpdated == null){
+      throw Exception('"onUpdated" callback in null!');
+    }
+    onUpdated!();
+  }
+
+  @protected
+   void onRefreshWidget(){
+    if(onUpdated == null){
+      throw Exception('"onUpdated" callback in null!');
+    }
+    onUpdated!();
+  }
+
+  @protected
+  IltvLogicIterableValue(super.initValue);
+}
---
+++ lib/logics/iltv_logic_iterable_value_not_safe.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:iltv/logics/iltv_logic_value_read_only.dart';
+
+class IltvLogicValueNotSafe<T> extends IltvLogicValueReadOnly<T>{ //Not generating exception when onUpdate == null 
+  set value(T newValue) {
+    isInitialized = true;
+    super.innerValue = newValue;
+    if(onUpdated != null){
+      onUpdated!();
+    }
+  }
+
+  @protected
+  void onRefreshWidget(){
+    if(onUpdated != null){
+      onUpdated!();
+    }
+  }
+
+  @protected
+  IltvLogicValueNotSafe(super.initValue);
+}
